75e18e7e60a68e8456f891056be02443380f3600
Tamara <choodo18@rambler.ru>
2022-06-26 23:43:54 +0700

    HomeWork250622

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// Task#2
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 ->  max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.Write("Введите первое целое число: ");
+// int NumberA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе целое число: ");
+// int NumberB = Convert.ToInt32(Console.ReadLine());
+
+// if(NumberA > NumberB)
+// {
+//    Console.WriteLine("Первое число больше второго");
+// }
+
+// else
+// {
+//    Console.WriteLine("Второе число больше первого");
+// }
+
+// Task#4
+// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.Write("Введите первое целое число: ");
+// int NumberA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе целое число: ");
+// int NumberB = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье целое число: ");
+// int NumberС = Convert.ToInt32(Console.ReadLine());
+// int max = NumberA;
+// if (NumberA > max) max = NumberA;
+// if (NumberB > max) max = NumberB;
+// if (NumberС > max) max = NumberС;
+
+// Console.Write("max= ");
+// Console.WriteLine(max);
+
+// Task#6
+// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите целое число: ");
+int numberА = Convert.ToInt32(Console.ReadLine());
+
+if (numberА % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+
+else
+{
+    Console.WriteLine("Число НЕ является четным");
+}
+
